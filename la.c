89ab96acd3a27bb097d356d5c3b254584de6eef8
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-02-17 22:08:19 -0500

    added tons of code

--- la.c
+++ la.c
@@ -7,6 +7,16 @@ struct graph {
   int* adj;
 };
 
+int nedges(struct graph g)
+{
+  return g.off[g.n];
+}
+
+int deg(struct graph g, int u)
+{
+  return g.off[u + 1] - g.off[u];
+}
+
 struct graph read_graph(FILE* f)
 {
   struct graph g;
@@ -16,7 +26,7 @@ struct graph read_graph(FILE* f)
   for (int i = 0; i < g.n; ++i)
     fscanf(f, "%d", g.off + i + 1);
   g.adj = malloc(sizeof(int) * g.off[g.n]);
-  for (int i = 0; i < g.off[g.n]; ++i)
+  for (int i = 0; i < nedges(g); ++i)
     fscanf(f, "%d", g.adj + i);
   return g;
 }
@@ -26,7 +36,7 @@ void write_graph(FILE* f, struct graph g)
   fprintf(f, "%d\n", g.n);
   for (int i = 0; i < g.n; ++i)
     fprintf(f, "%d\n", g.off[i + 1]);
-  for (int i = 0; i < g.off[g.n]; ++i)
+  for (int i = 0; i < nedges(g); ++i)
     fprintf(f, "%d\n", g.adj[i]);
 }
 
@@ -36,9 +46,81 @@ void free_graph(struct graph g)
   free(g.adj);
 }
 
+int la(struct graph g)
+{
+  int n = 0;
+  for (int u = 0; u < g.n; ++u) {
+    int a = g.off[u];
+    int b = g.off[u + 1];
+    for (int e = a; e < b; ++e)
+      if (g.adj[e] < u)
+        n += u - g.adj[e];
+  }
+  return n;
+}
+
+int edges_method(struct graph g)
+{
+  int n = g.n;
+  int m = nedges(g) / 2;
+  int i = 1;
+  int f = 0;
+  int lb = 0;
+  while (f + n - i <= m && i < n) {
+    f = f + n - i;
+    lb = lb + i * (n - i);
+    i = i + 1;
+  }
+  return lb + i * (m - f);
+}
+
+int degree_method(struct graph g)
+{
+  int lb = 0;
+  for (int u = 0; u < g.n; ++u) {
+    int d = deg(g, u);
+    if (d % 2 == 0)
+      lb = lb + (d * d) / 4 + d / 2;
+    else
+      lb = lb + (d * d + 2 * d + 1) / 4;
+  }
+  return lb / 2;
+}
+
+int* bfs(struct graph g)
+{
+  enum { BLACK, GREY, WHITE };
+  int* queue = malloc(sizeof(int) * g.n);
+  int* state = malloc(sizeof(int) * g.n);
+  int begin = 0;
+  int end = 0;
+  for (int i = 0; i < g.n; ++i)
+    state[i] = WHITE;
+  for (int i = 0; i < g.n; ++i)
+    if (state[i] == WHITE) {
+      queue[end++] = i;
+      while (end != begin) {
+        int j = queue[begin++];
+        state[j] = BLACK;
+        int a = g.off[j];
+        int b = g.off[j + 1];
+        for (int e = a; e < b; ++e) {
+          int k = g.adj[e];
+          state[k] = GREY;
+          queue[end++] = k;
+        }
+      }
+    }
+  free(state);
+  return queue;
+}
+
 int main()
 {
   struct graph g = read_graph(stdin);
-  write_graph(stdout, g);
+  printf("%d vertices, %d edges\n", g.n, nedges(g));
+  printf("current total spread: %d\n", la(g));
+  printf("lower bound by edges method: %d\n", edges_method(g));
+  printf("lower bound by degree method: %d\n", degree_method(g));
   free_graph(g);
 }
